39a437d30f62aec72ccb42b2afbe9a7e45451adf
Quan.NgoV1 <Quan.NgoV1@homecredit.com>
2024-04-15 23:16:14 +0700

    pump version

--- lib/ios_teamid.dart
+++ lib/ios_teamid.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+
 import 'ios_teamid_platform_interface.dart';
 
 class IosTeamid {
+  bool get isIOS => !kIsWeb && Platform.isIOS;
   Future<String?> getTeamId() async {
-    if (Platform.isIOS) {
+    if (isIOS) {
       return IosTeamidPlatform.instance.getTeamId();
     }
     return "";
